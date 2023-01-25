1a425b6ecbc9ede78224719cf9c5fa7042a259ca
Aleš Furman <afurman@nkbm.si>
2023-01-25 14:33:22 +0100

    ddasdsad

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>1f178a68-5a05-46b5-9776-d8eae6134492</testSuiteGuid>
+</TestSuiteEntity>
