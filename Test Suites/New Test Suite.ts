7af7f7b4a643752784ea0138ccb0a573e703ec55
Aleš Furman <afurman@nkbm.si>
2023-01-25 14:36:24 +0100

    mf.,m.,dsads

--- Test Suites/New Test Suite.ts
+++ Test Suites/New Test Suite.ts
@@ -5,10 +5,17 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
+   <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>1f178a68-5a05-46b5-9776-d8eae6134492</testSuiteGuid>
+   <testCaseLink>
+      <guid>89625d4f-afb5-472c-bccc-9a83f3f4ab42</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/New Test Case feri kontakit 2</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
